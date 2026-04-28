8c3669440ed8c31751f10c9b0559990a0b334d2f
ZiiroTech <info@ziirotech.com>
2026-04-28 09:22:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 
+async function loadSection(id, file) {
+  const res = await fetch(file);
+  const html = await res.text();
+  document.getElementById(id).innerHTML = html;
+}
+
+loadSection("hero", "sections/hero.html");
+loadSection("about", "sections/about.html");
+loadSection("services", "sections/services.html");
+loadSection("contact", "sections/contact.html");
